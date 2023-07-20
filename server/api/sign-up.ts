81f542aaca71adc7a649a8062608187f41fcbcc6
NM711 <nm7994918@gmail.com>
2023-07-19 21:37:41 -0400

    email sign-up added, but still need to add providers

--- server/api/sign-up.ts
+++ server/api/sign-up.ts
@@ -1,7 +1,36 @@
 import { supabase } from "../supabase"
+import { client } from "../../prisma/client"
 
-export default defineEventHandler((event) => {
-  console.log("hello world")
-  console.log(supabase)
+export default defineEventHandler( async (event) => {
+  const { res } = event.node
+  const body = await readBody(event)
+
+  const email = body.email
+  const username = body.username
+  const password = body.password
+
+  try {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password
+    })
+    const userData: any = data
+    const userId: any = userData.user.id
+    if (error) throw error
+
+    // creating user Identifier might be necessary if not tho ill just remove it
+    // i say this because supabase only has email field not a username one so i think this should
+    // be ok
+
+    const createUserIdentifier = await client.user_Identifier.create({
+      data: {
+        id: userId,
+        username
+      }
+    })
+
+  } catch (err) {
+    return res.end('Error Creating User!')
+  }
 })
 
