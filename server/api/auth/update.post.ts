8bfa198f234064d812b195903f58be82c2233ad3
NM711 <nm7994918@gmail.com>
2023-07-27 15:28:51 -0400

    fixed user update issue, and refactored the functions into a map!

--- server/api/auth/update.post.ts
+++ server/api/auth/update.post.ts
@@ -1,5 +1,6 @@
 import { supabase, checkSession, updateUserInfo } from "../../../supabase";
 import { updateUserIdentityUsername } from "../../../prisma/querys/user";
+import {object} from "yup";
 
 // will be properly tested once ui guys can build me a form since supabase adds the token to localstorage
 
@@ -8,19 +9,51 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   console.log(body)
   const { username, email, password } = body
-  
+  console.log(username, email, password)
+  const checkEmailMessage: string = `Verification email has been sent to new email of ${email}`
   try {
-      await checkSession(event)
-      if (email && password) {
-        await updateUserInfo({ email, password })
-      } else if (email) {
-        await updateUserInfo({ email })
-      } else if (password) {
-        await updateUserInfo({ password })
-      } else if (email && username && password) {
-        const { user } = await updateUserInfo({ email, password })
-        await updateUserIdentityUsername(user.id, username)
+      const { user }: any = await checkSession(event)
+      // some sub funcions can probably be refactored but this is readable enough for now i think...
+      const actions = {
+        emailAndPassword: async () => {
+          await updateUserInfo({ email, password })
+          return checkEmailMessage
+        },
+        emailAndUsername: async () => {
+          await updateUserInfo({ email })
+          await updateUserIdentityUsername(user.id, username)
+        },
+        email: async () => {
+          await updateUserInfo({ email })
+          return checkEmailMessage
+        },
+        password: async () => {
+          await updateUserInfo({ password })
+        },
+        username: async () => {
+          await updateUserIdentityUsername(user.id, username)
+        },
+        all: async () => {
+          await updateUserInfo({ email, password })
+          await updateUserIdentityUsername(user.id, username)
+          return checkEmailMessage
+        }
       }
+
+      if (email && password) {
+        actions.emailAndPassword()
+      } else if (email && password && username) {
+        actions.all()
+      } else if (email && username) {
+        actions.emailAndUsername()
+      } else if (email) {
+        actions.email()
+      } else if (username) {
+        actions.username()
+      } else if (password) {
+        actions.password()
+      }
+
   } catch (err) {
     return res.end('Error Updating User!') 
   }
