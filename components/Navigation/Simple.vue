03808f6afc723b2479db553789ef60151e4970b8
Hessel <49622357+Heesel@users.noreply.github.com>
2023-07-27 18:13:39 +0200

    Refactored home page + favicon

--- components/Navigation/Simple.vue
+++ components/Navigation/Simple.vue
@@ -1,27 +1,54 @@
 <template>
-    <div class="w-full bg-[#b255ff]">
-        <nav aria-label="navigation" class="container mx-auto flex max-w-7xl items-center justify-between p-4 ">
-            <a aria-label="logo" href="/" class="inline-flex items-center justify-center rounded-lg">
-                <span class="text-white font-bold text-3xl">Sticky Toads</span>
-            </a>
-
-            <ul class="flex items-center gap-2 text-sm font-medium text-white">
-
-                <li class="rounded-lg px-3 py-2 hover:text-green-500 ease-in duration-200 hover:cursor-pointer" aria-label="sign-up">
-                    <NuxtLink class="border-2 border-cyan-400 flex items-center gap-1 transition-colors ease-in-out duration-150 text-white font-bold py-2 px-3 w-26 rounded-md">
-                        <span>Sign-up</span>  
-                    </NuxtLink>          
-                </li>
-
-                <li class="px-3 py-2" aria-label="sign-in">
-                    <ButtonPrimaryLinkWithArrow text="Sign-in" route="/auth/sign-in" />
-                </li>
-
-            </ul>
-        </nav>
+    <div class="bg-[#b255ff]">
+    <div class="container px-4 mx-auto">
+      <div class="flex items-center justify-between py-5">
+        <div class="w-auto">
+          <div class="flex flex-wrap items-center">
+            <div class="w-auto pr-2">
+              <NuxtLink to="/">
+                <span style="font-family: 'Permanent Marker', cursive;" class="text-white text-4xl">Sticky Toads</span>
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+        <div class="w-auto">
+          <div class="flex flex-wrap items-center gap-6">
+            <div class="w-auto hidden lg:block">
+                <NuxtLink class="border-2 border-cyan-400 flex items-center gap-1 transition-colors ease-in-out duration-150 text-white font-bold py-3 px-3 w-26 rounded-md">
+                    <span>Sign-in</span>  
+                </NuxtLink>   
+            </div>
+            <div class="w-auto hidden lg:block">
+              <div class="inline-block">
+                <ButtonPrimaryLinkWithArrow text="Sign-up" route="/auth/sign-up"/>
+              </div>
+            </div>
+            <div class="relative w-auto lg:hidden">
+                <div @click="visible = !visible">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                </div>      
+            </div>
+            <div :class="visible ? 'absolute' : 'hidden'" class="z-10 bg-gray-800 text-white py-3 px-12 right-6 top-12 rounded">
+                <ul class="divide-y divide-gray-700/100">
+                    <li class="py-1">
+                        <NuxtLink to="/auth/sign-in">Sign-in</NuxtLink>
+                    </li>
+                    <li class="py-1">
+                        <NuxtLink to="/auth/sign-up">Sign-up</NuxtLink>
+                    </li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
+const visible = ref(false)
 </script>
 <style scoped>
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,12 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
 
       ], 
-      link: [],
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: "anonymous" },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lexend:wght@300;400&display=swap' },
+      ],
       script: []
     },
   },
